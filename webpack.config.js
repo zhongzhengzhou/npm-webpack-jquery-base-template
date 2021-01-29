0cf4487bf5be379a626def7fd5b49db4491798df
周中正 <48094840+zhongzhengzhou@users.noreply.github.com>
2021-01-29 10:16:21 +0800

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
+const webpack = require('webpack')
 
 module.exports = {
   entry: "./src/main.js",
